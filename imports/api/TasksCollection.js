cd59e50f9ef9b4435817b1ed2fac984a6ed2ec2d
jkronlachner <julian.kronlachner@lab73.at>
2023-06-18 14:06:10 +0200

    tutorialized app

--- imports/api/TasksCollection.js
+++
@@ -1,4 +0,0 @@
-import {Mongo} from "meteor/mongo";
-
-//STEP 3: Create a new Mongo collection
-export const TasksCollection = new Mongo.Collection("tasks");
--- server/main.js
+++ server/main.js
@@ -1,20 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import {TasksCollection} from "../imports/api/TasksCollection";
 
-const insertTask = taskText => TasksCollection.insert({ text: taskText });
 
 Meteor.startup(async () => {
-  //STEP 4: SEED DATE ON STARTUP
-  //if the tasks collection is empty, create some data
-  if(TasksCollection.find().count() === 0) {
-    [
-      'First Task',
-      'Second Task',
-      'Third Task',
-      'Fourth Task',
-      'Fifth Task',
-      'Sixth Task',
-      'Seventh Task'
-    ].forEach(insertTask)
-  }
 });
